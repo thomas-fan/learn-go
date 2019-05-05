dcc6ff78d711fa832a32829c64d473fc060a199d
thomas-fan <youxiboy007@vip.qq.com>
2019-05-05 14:27:01 +0800

    学习for循环，整理目录结构

---
+++ basic/loop.go
@@ -0,0 +1,42 @@
+package basic
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func convertToBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+func printFile(filename string) {
+	file, err:= os.Open(filename)
+	if err != nil {
+		panic(nil)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func forever()  {
+	for {
+		fmt.Println("abc")
+	}
+}
+
+func main() {
+	fmt.Println(
+		convertToBin(5),
+		convertToBin(13))
+	printFile("abc.txt")
+	//forever()
+
+}
