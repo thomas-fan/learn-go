6af8e8e8f0682133c05f15afd5d364a0a5e2110f
thomas-fan <youxiboy007@vip.qq.com>
2019-05-06 17:05:37 +0800

    通过 interface{}代表范型

--- interface/retrieiver/main.go
+++ interface/retrieiver/main.go
@@ -4,17 +4,38 @@ import (
 	"azsy.cn/thomas-fan/go-demo/interface/retrieiver/mock"
 	"azsy.cn/thomas-fan/go-demo/interface/retrieiver/real"
 	"fmt"
+	"time"
 )
 
 type Retriever interface {
 	Get(url string) string
 }
+
 func download(r Retriever) string {
 	return r.Get("http://www.baidu.com")
 }
 func main() {
 	var r Retriever
 	r = mock.Retriever{"this is mock data"}
-	r = real.Retriever{}
-	fmt.Println(download(r))
+	inspect(r)
+	r = &real.Retriever{
+		UserAgent: "iPhone",
+		TimeOut:   time.Minute,
+	}
+	inspect(r)
+	if realRetriever,ok := r.(*real.Retriever); ok {
+		fmt.Println(realRetriever.TimeOut)
+	} else {
+		fmt.Println("not real retriever")
+	}
+}
+
+func inspect(r Retriever) {
+	fmt.Printf("%T %v\n", r, r)
+	switch v := r.(type) {
+	case mock.Retriever:
+		fmt.Println("Contents: ", v.Contents)
+	case *real.Retriever:
+		fmt.Println("UserAgent: ", v.UserAgent)
+	}
 }
--- oop/queue/entry/entry.go
+++ oop/queue/entry/entry.go
@@ -18,7 +18,10 @@ func main() {
 	q := queue.Queue{1}
 	q.Push(2)
 	q.Push(3)
+	q.Push("abc")
 	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
 	fmt.Println(q.Pop())
 	fmt.Println(q.IsEmpty())
 	fmt.Println(q.Pop())
